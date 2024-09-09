b27551fab0fe030650d92ffcb526996f1db0d2bd
Hussain Yousuf Sunain <webhys2003@gmail.com>
2024-09-09 18:47:04 +0500

    first commit

---
+++ activity.c
@@ -0,0 +1,77 @@
+// print you name in UPPERCASE and lowercase
+// print you name first latter in scanf
+
+// #include<stdio.h>
+
+// void main(){
+//     printf("MOAZ bin amir");
+// }
+
+// #include <stdio.h>
+
+// void main()
+// {
+//     char name; // variable declare with the name of "name".
+//     printf("Enter your name first character: "); // this is for showing for getting the user name first character.
+//     scanf("%c", &name); // this for getting name first character
+//     printf("your name first character is: %c", name); // this is for print final result
+// }
+
+// #include <stdio.h>
+
+// void main()
+// {
+//     char name;
+//     printf("Enter your name: ");
+//     scanf("%c", &name);
+//     printf("you name first character is %c", name);
+// }
+
+// #include <stdio.h>
+// #include <conio.h>
+
+// void main()
+// {
+//     char key;
+//     printf("Enter any key ");
+//     key = getch();
+// }
+
+// #include<stdio.h>
+
+// void main(){
+//     char name[] = "hudair";
+//     printf("%s", name);
+// }
+
+
+#include<stdio.h>
+
+void main(){
+    // printf("Name: \tAli\nFname: \tHudair\nMarks: \t1000");
+    // printf("Name: Ali\nFname: Hudair\nMarks: 1000");
+    // variable
+    // int sum = 6;
+    // sum = 6
+    // printf("%d", sum);
+    // int var = sum;
+    // int a = var;
+    // printf("%d", a);
+    // int var = sum;
+
+
+    int a = 2, b = 3, temp;
+    temp = a; // tempo = 2
+    a = b; // a = 3
+    b = temp; // b = 2
+    printf("value of after swapping a: %d\n", a);
+    printf("value of after swapping b: %d\n", b);
+}
+
+// 10 = 10 = true
+
+// Arithmetic operator
+// +
+// -
+// *
+// /
